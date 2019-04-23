5d7aeff49ff10024dbfc9449fc444004d6c4512b
GrozniyDave <38036338+GrozniyDave@users.noreply.github.com>
2019-04-23 09:58:53 +0200

    Updates

---
+++ precache-manifest.471ac396578fb7770abe8b93f648a148.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c0ec12a0584b24947a7",
+    "url": "/TwitterHashtagSearch/static/js/runtime~main.6114db1e.js"
+  },
+  {
+    "revision": "5a1c4f4ba71d6b10ead7",
+    "url": "/TwitterHashtagSearch/static/js/main.64d0406f.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/TwitterHashtagSearch/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5a1c4f4ba71d6b10ead7",
+    "url": "/TwitterHashtagSearch/static/css/main.98d747e5.chunk.css"
+  },
+  {
+    "revision": "ed2cbe3b53a7a547499e9a1211371341",
+    "url": "/TwitterHashtagSearch/index.html"
+  }
+];
